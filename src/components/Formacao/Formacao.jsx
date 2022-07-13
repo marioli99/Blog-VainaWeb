a168c568ee8c90988e98df737ea9c238425d355a
marioli99 <mazmariana.mo@gmail.com>
2022-07-12 21:07:47 -0300

    Iniciando Formacao

--- src/components/Formacao/Formacao.jsx
+++ src/components/Formacao/Formacao.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 
+import { ContainerFormacao, Text, MiniTitle, MaxTitle, MiniImg, MaxImg,ContainerText  } from "./FormacaoStyle";
+
 export default function Formacao () {
     return (
-        <>
-        <h2>Formação</h2>
-        <></>
-        <h3>FRONT-END</h3>
-        <p>Quem trabalha com Front End é responsável por desenvolver por meio do código uma interface gráfica e, normalmente, com as tecnologias base da Web: HTML, CSS e JavaScript.</p>
+        <ContainerFormacao>
+            <ContainerText>
+            <MaxTitle>Formação</MaxTitle>
+        <MiniImg></MiniImg>
+        <MiniTitle>FRONT-END</MiniTitle>
+        <Text>Quem trabalha com Front End é responsável por desenvolver por meio do código uma interface gráfica e, normalmente, com as tecnologias base da Web: HTML, CSS e JavaScript.</Text>
 
-        <></>
-        <h3>BACK-END</h3>
-        <p>O Back-End trabalha em boa parte dos casos fazendo a ponte entre os dados que vem do navegador rumo ao banco de dados e vice-versa, sempre aplicando as devidas regras de negócio, validações e garantias num ambiente restrito ao usuário final (ou seja, onde ele não consegue acessar ou manipular algo).</p>
+        <MiniImg></MiniImg>
+        <MiniTitle>BACK-END</MiniTitle>
+        <Text>O Back-End trabalha em boa parte dos casos fazendo a ponte entre os dados que vem do navegador rumo ao banco de dados e vice-versa, sempre aplicando as devidas regras de negócio, validações e garantias num ambiente restrito ao usuário final (ou seja, onde ele não consegue acessar ou manipular algo).</Text>
+            </ContainerText>
+       
 
-        <></>
-        </>
+        <MaxImg></MaxImg>
+        </ContainerFormacao>
     )
 }
---
+++ src/components/Formacao/FormacaoStyle.js
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const ContainerFormacao = styled.div`
+  display: flex;
+`;
+
+export const ContainerText = styled.div`
+  margin-top: 5%;
+  margin-left: 10%;
+`;
+
+export const MaxTitle = styled.h2`
+  font-weight: bold;
+  color: #0f2260;
+`;
+
+export const MiniTitle = styled.h3`
+  color: #0f2260;
+`;
+
+export const Text = styled.p`
+  font-size: 14px;
+  width: 25vw;
+`;
+
+export const MiniImg = styled.div`
+  width: 20vw;
+  height: 10vh;
+  background-color: antiquewhite;
+  display: flex;
+`;
+
+export const MaxImg = styled.div`
+  width: 40vw;
+  background-color: firebrick;
+  height: 60vh;
+  margin: auto;
+`;
